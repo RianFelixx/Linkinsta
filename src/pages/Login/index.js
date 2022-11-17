259987c4411b8d3a959de4317908962077fa442d
Taebinn <rfo@etepd.com>
2022-11-17 00:35:22 -0300

    Criação, estilização e configuração do login da page login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,58 @@
-export default function Login(){
-    return(
-        <div>
-            <h1>Página de Login</h1>
+import { useState } from 'react'
+import './login.css'
+import { Logo } from '../../components/Logo'
+import { auth } from '../../services/firebaseConnection'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate} from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const navigate = useNavigate();
+
+    function handleLogin(e){
+        e.preventDefault();
+
+        if(email === '' || password === ''){
+            alert("Preencha todos os campos!")
+            return;
+        } 
+
+
+        signInWithEmailAndPassword(auth, email, password).then(() => {
+            toast.success("Bem vindo de volta :)")
+            navigate("/admin", { replace: true} );
+        }).catch(() => {
+            toast.error("Email e/ou Senha invalidos!")
+            console.log("Email e/ou Senha invalidos!")
+        })
+    }
+
+    return (
+        <div className='login-container'>
+            <Logo />
+
+            <form className='form' onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder='Digite seu E-mail'
+                    value={email}
+                    onChange={ (e) => setEmail(e.target.value)}
+                />
+
+                <input
+                    type="password"
+                    placeholder='Digite sua Senha'
+                    autoComplete='on'
+                    value={password}
+                    onChange={ (e) => setPassword(e.target.value)}
+                />
+
+                <button type='submit'>Acessar</button>
+            </form>
         </div>
     )
 }
