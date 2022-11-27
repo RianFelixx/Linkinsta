a78513f352bb884039b7eb74a8d02bff5a26b85d
Taebinn <rfo@etepd.com>
2022-11-27 14:53:20 -0300

    your connection

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -3,14 +3,8 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8UJ-bMLq4niBO9wLj3UyslBU3XemV3XI",
-  authDomain: "linkinsta-68f7b.firebaseapp.com",
-  projectId: "linkinsta-68f7b",
-  storageBucket: "linkinsta-68f7b.appspot.com",
-  messagingSenderId: "531114584981",
-  appId: "1:531114584981:web:9f340e48a9ab47070fe49c",
-  measurementId: "G-1JNM3GYSWZ"
-};
+  //your connection firebase
+}; 
 
 const firebaseApp = initializeApp(firebaseConfig);
 
